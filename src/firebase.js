aaaac1ac4b6ca16ee144263aecbcaea077744bde
jojo114 <68749841+jojo114@users.noreply.github.com>
2022-07-25 18:35:48 +0100

    installed redux library

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDu5Ocj_0aY4L11jpA3R-TnyFafVfQYLec",
+    authDomain: "netflix-clone-5b196.firebaseapp.com",
+    projectId: "netflix-clone-5b196",
+    storageBucket: "netflix-clone-5b196.appspot.com",
+    messagingSenderId: "856603935346",
+    appId: "1:856603935346:web:905928366a9c52e96e38eb"
+  };
