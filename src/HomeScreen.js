dc7f163fcd57fb0142136c6bb6907c29b9b511d5
jojo114 <68749841+jojo114@users.noreply.github.com>
2022-07-27 23:41:29 +0100

    created a row component  for the movies and styled it
    created a row component for the different movies genre and added movies , styled the row component to display flex and overflow-x scroll

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -12,29 +12,29 @@ const HomeScreen = ()=> {
 
         <Row
         title='NETFLIX ORIGINALS'
-        fetchUrl = {`/discover/tv?api_key=8e42ee1a9479f29a21cc0807854b49e5&with_networks=213`}
+        fetchUrl = {"/discover/tv?api_key=8e42ee1a9479f29a21cc0807854b49e5&with_networks=213"}
         isLargeRow />
         <Row
         title='Trending Now'
-        fetchUrl = {`/trending/all/week?api_key=8e42ee1a9479f29a21cc0807854b49e5&language=en-US`} />
+        fetchUrl = {"/trending/all/week?api_key=8e42ee1a9479f29a21cc0807854b49e5&language=en-US"} />
         <Row
          title='Top Rated'
-         fetchUrl ={ `/movie/top_rated?api_key=8e42ee1a9479f29a21cc0807854b49e5&language=en-US`} />
+         fetchUrl ={ "/movie/top_rated?api_key=8e42ee1a9479f29a21cc0807854b49e5&language=en-US"} />
         <Row
          title='Action Movies'
-         fetchUrl ={  `/discover/movie?api_key=8e42ee1a9479f29a21cc0807854b49e5&with_genres=28`} />
+         fetchUrl ={  "/discover/movie?api_key=8e42ee1a9479f29a21cc0807854b49e5&with_genres=28"} />
         <Row
          title='Comedy Movies'
-         fetchUrl ={ `/discover/movie?api_key=8e42ee1a9479f29a21cc0807854b49e5&with_genres=35`} />
+         fetchUrl ={ "/discover/movie?api_key=8e42ee1a9479f29a21cc0807854b49e5&with_genres=35"} />
         <Row
          title='Horror Movies'
-         fetchUrl ={ `/discover/movie?api_key=8e42ee1a9479f29a21cc0807854b49e5&with_genres=27`} />
+         fetchUrl ={ "/discover/movie?api_key=8e42ee1a9479f29a21cc0807854b49e5&with_genres=27"} />
         <Row
          title='Romance Movies'
-         fetchUrl ={ `/discover/movie?api_key=8e42ee1a9479f29a21cc0807854b49e5&with_genres=10749`} />
+         fetchUrl ={ "/discover/movie?api_key=8e42ee1a9479f29a21cc0807854b49e5&with_genres=10749"} />
         <Row
          title='Documentaries'
-         fetchUrl ={ `/discover/movie?api_key=8e42ee1a9479f29a21cc0807854b49e5&with_genres=99`} />
+         fetchUrl ={ "/discover/movie?api_key=8e42ee1a9479f29a21cc0807854b49e5&with_genres=99"} />
 
       
     </div>
--- src/Row.js
+++ src/Row.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import './Row.css';
 
-const Row = ({title,fetchUrl , isLargeRow = false}) => {
+const Row = ({title, fetchUrl , isLargeRow = false}) => {
     const [movies,setMovies] = useState([]);
 
 
 
     useEffect(()=>{
     async function getData(){
-        const response = await fetch(`${fetchUrl}`);
-        const data = response.json();
+        const response = await fetch(`https://api.themoviedb.org/3${fetchUrl}`);
+        const data = await response.json();
         setMovies(data.results);
         return response;
     }
@@ -19,11 +19,13 @@ const Row = ({title,fetchUrl , isLargeRow = false}) => {
   return (
     <div className='row'>
       <h2>{title}</h2>
-      {movies.map((movie)=>{
-        return <img src={`"https://image.tmdb.org/t/p/original/${isLargeRow ? movie.backdrop_path : movie.backdrop_path}"`} alt={movies.name} />
-      })}
+      <div className="row_posters">
+          {movies.map((movie)=>{
+            return <img  key={movie.id} className={`row_poster ${isLargeRow && "row_posterLarge"}`} src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
+          })}
+      </div>
     </div>
   )
-}
+} 
 
 export default Row
