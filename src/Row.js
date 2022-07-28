f0660ed10bec7135f0c5fc007d2aaf63ce8d69fc
jojo114 <68749841+jojo114@users.noreply.github.com>
2022-07-28 18:33:08 +0100

    Update Row.js
    updated the images in the row  component  so as not to display dead links images

--- src/Row.js
+++ src/Row.js
@@ -20,9 +20,10 @@ const Row = ({title, fetchUrl , isLargeRow = false}) => {
     <div className='row'>
       <h2>{title}</h2>
       <div className="row_posters">
-          {movies.map((movie)=>{
-            return <img  key={movie.id} className={`row_poster ${isLargeRow && "row_posterLarge"}`} src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
-          })}
+          {movies.map((movie)=>(
+              ((isLargeRow && movie.poster_path) || (!isLargeRow && movie.backdrop_path))&&(
+              <img  key={movie.id} className={`row_poster ${isLargeRow && "row_posterLarge"}`} src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
+          )))}
       </div>
     </div>
   )
