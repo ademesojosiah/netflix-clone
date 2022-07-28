6d3cf424b9ae4613ebbbf5bac3d7c3ebacab3a55
jojo114 <68749841+jojo114@users.noreply.github.com>
2022-07-28 23:41:24 +0100

    installed router to navigate to different screens

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screens/HomeScreen';
+
 
 
 function App() {
+  const user = null
   return (
     <div className="app">
-      <HomeScreen/>
+    <Router>
+        <Routes>
+          <Route path="/" element={<HomeScreen/>} />
+        </Routes>
+    </Router>
+
     </div>
+  
+    
   );
 }
 
