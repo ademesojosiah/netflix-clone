7c0331e8a4d4332c7e1a0330cb26fd55e9f1499b
jojo114 <68749841+jojo114@users.noreply.github.com>
2022-08-03 13:39:59 +0100

    catch the error, styled the profileScreen
    catch the error in the signupscreen and diplayed it in the form

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,33 +1,38 @@
 import { auth } from '../firebase.js'
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import './SignupScreen.css';
 import HomeScreen from './HomeScreen';
 
 
 const SignupScreen = () => {
+    const [signInError, setsignInError] = useState('');
     const emailRef = useRef(null);
     const passwordRef = useRef(null);
 
     function signIn(e){
         e.preventDefault();
+        
+        (emailRef.current.value == '' || passwordRef.current.value == '')?( setsignInError('the email or the password is empty')):(
         auth.signInWithEmailAndPassword(emailRef.current.value ,passwordRef.current.value)
         .then((authUser)=>{
             return <HomeScreen />
         }).catch((error)=>{
-            alert(error.message)
+            setsignInError(error.message);
         })
-        
+        )
     }
 
     function register(e){
         e.preventDefault();
 
+        (emailRef.current.value == '' || passwordRef.current.value == '')?( setsignInError('the email or the password is empty')):(
         auth.createUserWithEmailAndPassword(emailRef.current.value ,passwordRef.current.value)
         .then((authUser)=>{
-            console.log(authUser)
+            setsignInError('click sign in')
         }).catch((error)=>{
-            alert(error.message);
+            setsignInError(error.message);
         })
+        )
 
     
     };
@@ -39,6 +44,7 @@ const SignupScreen = () => {
         <h1>Sign In</h1>
         <input ref={emailRef} type='email' placeholder='Email' />
         <input ref={passwordRef} type='password' placeholder='Password' />
+        <h6>{signInError}</h6>
         <button type='submit' onClick={signIn}>Sign In</button>
 
         <h4><span className='signupScreen_grey'>New to Netflix?</span> <span className="signupScreen_link" onClick={register}>Sign Up now.</span></h4>
